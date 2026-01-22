a6052a2c21cd323e7cd415e2e0e8fd854bd6f7ef
husanahmedov <mr.southkoreakhusan@gmail.com>
2026-01-22 23:54:28 +0900

    --amend

--- apps/nestar-batch/src/nestar-batch.service.ts
+++ apps/nestar-batch/src/nestar-batch.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class NestarBatchService {
-	getHello(): string {
-		return 'Welcome to Nestar Batch Service!';
-	}
-
 	public async batchRollback(): Promise<void> {
 		console.log('Batch rollback executed');
 	}
@@ -14,5 +10,8 @@ export class NestarBatchService {
 	}
 	public async batchTopAgents(): Promise<void> {
 		console.log('Batch top agents executed');
+	}
+	public getHello(): string {
+		return 'Welcome to Nestar Batch Service!';
 	}
 }
